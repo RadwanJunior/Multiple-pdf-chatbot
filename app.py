7cd1aa67102d6ce017437df16891d422bbd3b158
Adham Radwan <aradw009@uottawa.ca>
2024-02-22 19:12:00 -0500

    Feature - Added ability to seperate text from pdf into chunks that will be returned by a function as a list

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from dotenv.main import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -9,6 +12,16 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def main():
     load_dotenv()
@@ -28,9 +41,9 @@ def main():
             with st.spinner("Processing"):
                 #get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
 
                 # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
 
                 #create vector store
 
